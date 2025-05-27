50cdb97aeeb481e4f1b923d9829a64746ab23c54
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-05-27 17:31:17 -0300

    feat: extend ServerPropsInterface with get and post method signatures

--- backend/src/types/server-props-interface.ts
+++ backend/src/types/server-props-interface.ts
@@ -1,5 +1,10 @@
+import type { ServerRequestInterface } from "./server-request-interface"
+import type { ServerReplyInterface } from "./servers-reply"
+
 export interface ServerPropsInterface {
     register: (plugin: any, opts?: any) => any
     listen: (opts: { port: number },
         callback?: (err: Error | null, address?: string) => void) => void
+    get: (path: string, config: (request: ServerRequestInterface, reply: ServerReplyInterface) => any) => any
+    post: (path: string, config: (request: ServerRequestInterface, reply: ServerReplyInterface) => any) => any
 }
