b3e9f5720a36b3b40e01de734a9d8157c0c19e55
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-05-28 20:41:25 -0300

    feat: enhance buildApp function to support rate limiting and listen port configuration

--- backend/src/build-app.ts
+++ backend/src/build-app.ts
@@ -1,12 +1,20 @@
-import type { ServerPropsInterface } from "./types/server-props-interface"
+import type { BuildAppPropsInterface } from "./types/build-app-props-interface"
 
-export const buildApp = async ({ app, cors, routes }:
-    { app: ServerPropsInterface, cors: Function, routes: Function }): Promise<void> => {
+export const buildApp = async ({
+    app,
+    cors,
+    rateLimit,
+    routes,
+    listenPort
+}: BuildAppPropsInterface): Promise<void> => {
     try {
         await app.register(cors)
+        if (rateLimit) {
+            await app.register(rateLimit.instance, rateLimit.options)
+        }
         await app.register(routes)
-        app.listen({ port: Number(process.env.SERVER_PORT) || 3000 })
 
+        app.listen({ port: listenPort || Number(process.env.SERVER_PORT) || 3000 })
     } catch (error) {
         process.exit(1)
     }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,46 @@ import cors from "@fastify/cors"
 import routes from "./routes"
 import { buildApp } from "./build-app"
 import type { ServerPropsInterface } from "./types/server-props-interface"
+import mongoose from "mongoose"
+import rateLimit from "@fastify/rate-limit"
 
 configDotenv()
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL não definida no .env")
+}
+
+async function connectDatabase() {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL as string)
+        console.log("MongoDB conectado!")
+    } catch (error) {
+        console.error("Erro ao conectar ao MongoDB:", error)
+        process.exit(1)
+    }
+}
+
 const app = fastify({ logger: true }) as unknown as ServerPropsInterface
-buildApp({ app, cors, routes })
+
+const rateLimitConfig = {
+    instance: rateLimit,
+    options: {
+        max: 100,
+        timeWindow: "1 second"
+    }
+}
+
+buildApp({
+    app,
+    cors,
+    routes,
+    rateLimit: rateLimitConfig,
+    listenPort: Number(process.env.SERVER_PORT) || 3000
+})
+
+connectDatabase()
+
+process.on("SIGINT", async () => {
+    await mongoose.disconnect()
+    process.exit(0)
+})
