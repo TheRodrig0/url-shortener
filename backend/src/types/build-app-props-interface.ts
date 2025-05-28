3c494969fb0c5d3a361e6b22b416b8bcd384d1c5
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-05-28 20:48:17 -0300

    feat: create BuildAppPropsInterface for application configuration

---
+++ backend/src/types/build-app-props-interface.ts
@@ -0,0 +1,12 @@
+import { ServerPropsInterface } from "./server-props-interface"
+
+export interface BuildAppPropsInterface {
+    app: ServerPropsInterface
+    cors: any
+    routes: any
+    listenPort?: number
+    rateLimit?: {
+        instance: any
+        options: object
+    }
+}
