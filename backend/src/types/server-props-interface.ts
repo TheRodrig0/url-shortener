b5464aa4ebf8f54738c1d501db9134b2ed2fec35
TheRodrig0 <rodrigogeronimo11@gmail.com>
2025-05-30 14:50:08 -0300

    feat: add 'ready' and 'server' properties to ServerPropsInterface interface

--- backend/src/types/server-props-interface.ts
+++ backend/src/types/server-props-interface.ts
@@ -1,5 +1,6 @@
 import type { ServerRequestInterface } from "./common/server-request-interface"
 import type { ServerReplyInterface } from "./common/server-reply-interface"
+import { Server } from "http"
 
 export interface ServerPropsInterface {
     register: (plugin: any, opts?: any) => any
@@ -7,4 +8,6 @@ export interface ServerPropsInterface {
         callback?: (err: Error | null, address?: string) => void) => void
     get: (path: string, config: (request: ServerRequestInterface, reply: ServerReplyInterface) => any) => any
     post: (path: string, config: (request: ServerRequestInterface, reply: ServerReplyInterface) => any) => any
+    ready: () => Promise<void>
+    server: Server
 }
